718670d45085df29306b2e09d84a72e34ea80351
srikanthAkiti <akitisrikanth1999@gmail.com>
2024-11-12 16:18:17 -0600

    new project

---
+++ CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace CollegeApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StudentController: ControllerBase
+	{
+		[HttpGet]
+		public string GetStudentName()
+		{
+			return "student name 1";
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
